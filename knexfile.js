4be3e3ef11fa9a474f48471cdce89e67f4e9969f
timohuennebeck <hello@timohuennebeck.com>
2022-11-23 22:07:29 +0100

    add database data to env file

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
     client: "mysql2",
     connection: {
-        host: "127.0.0.1",
-        user: "root",
-        password: "JL:1590!aus",
-        database: "alia_api",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         charset: "utf8",
     },
 };
